937d396ba0a91a26114befba3da0f60361920d14
donuwave <d_tareev@mail.ru>
2025-12-05 18:18:34 +0300

    ref: вернул конфиг

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "@app/index.tsx";
 import "@fontsource/geologica/400.css";
 import "@fontsource/geologica/500.css";
 import "@fontsource/dela-gothic-one/400.css";
+import {App} from "./app";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
